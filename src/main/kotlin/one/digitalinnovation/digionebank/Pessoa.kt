6c7c6a8d430d9318715cc21c1874304d70cd355d
razolet <rafaelzolet@yahoo.com.br>
2022-07-11 20:10:38 -0300

    Membros das classes na pratica

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,12 +3,26 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Rafael"
     var cpf: String = "123.123.123-11"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
+
+/*    inner class Endereco {
+        var rua: String = "Rua Teste"
+    }*/
 }
 
 fun main() {
     val rafael = Pessoa()
+//    rafael.cpf = "4567"
 
+/*    println(rafael)
     println(rafael.nome)
-    println(rafael.cpf)
+    println(rafael.cpf)*/
+    println(rafael.pessoaInfo())
 
+//    println(rafael.Endereco().rua)
 }
