60f66d2e307236bb97cdb2d096d6804cf1ccedcd
razolet <rafaelzolet@yahoo.com.br>
2022-07-11 21:49:41 -0300

    Introdução a abstrações - parte 1

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main() {
+    val rafael = Pessoa(nome = "Rafael", cpf = "123456789-10")
+
+    println(rafael.nome)
+    println(rafael.cpf)
+
+}
